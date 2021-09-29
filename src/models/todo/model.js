671aef35f328c9be9bad59220a7d19c5641daac8
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-09-29 19:10:41 +0300

    ..

--- src/models/todo/model.js
+++ src/models/todo/model.js
@@ -3,8 +3,8 @@
 const todoModel = (sequelize, DataTypes) => sequelize.define('todo', {
   toDoItem: { type: DataTypes.STRING, required: true },
   assignedTo: { type: DataTypes.STRING, required: true },
-  difficulty: { type: DataTypes.INTEGER, required: true },
-  complete: { type: DataTypes.BOOLEAN, required: true },
+  difficulty: { type: DataTypes.INTEGER, required: true,defaultValue:3 },
+  complete: { type: DataTypes.BOOLEAN, required: true,defaultValue:false },
 });
 
 module.exports = todoModel;
