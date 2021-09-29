b074085aea21bb7173e2fb6292a5e148eb177023
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-09-29 16:22:28 +0300

    model

--- src/models/todo/model.js
+++ src/models/todo/model.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const todoModel = (sequelize, DataTypes) => sequelize.define('todo', {
-  text: { type:DataTypes.STRING, required:true },
-  category: {type: DataTypes.STRING},
-  assignee: { type:DataTypes.STRING, required:true },
-  difficulty: {type:DataTypes.INTEGER, required:true, default: 3},
-  complete: {type:DataTypes.BOOLEAN, required:true, default:false},
+  toDoItem: { type: DataTypes.STRING, required: true },
+  assignedTo: { type: DataTypes.STRING, required: true },
+  difficulty: { type: DataTypes.INTEGER, required: true, default: 3 },
+  complete: { type: DataTypes.BOOLEAN, required: true, default: false },
 });
 
 module.exports = todoModel;
